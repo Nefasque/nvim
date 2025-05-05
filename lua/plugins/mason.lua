69372637d4ee9574048f7047306d9607078b7953
Ferchupessoadev <fernandomatiaspessoa471@gmail.com>
2025-05-05 17:08:08 -0300

    add(lspconfig): add lsp config for autocomplete

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,48 +1,55 @@
 return {
-  "williamboman/mason.nvim",
-  enabled = true,
-  event = "VeryLazy",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
-  opts = {
-    ui = {
-      border = 'single',
-      icons = {
-        package_pending = "  ",
-        package_installed = " 󰄳 ",
-        package_uninstalled = " 󰚌 ",
-      },
-      keymaps = {
-        toggle_server_expand = "<CR>",
-        install_server = "i",
-        update_server = "u",
-        check_server_version = "c",
-        update_all_servers = "U",
-        check_outdated_servers = "C",
-        uninstall_server = "X",
-        cancel_installation = "<C-c>",
-      },
-    },
-    max_concurrent_installers = 10,
-  },
-  config = function()
-    require("mason").setup()
-    local ensure_installed = {}
-    vim.list_extend(ensure_installed, {
-      "stylua",
-    })
-    require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "lua_ls",
-        "ts_ls",
-        "pylsp",
-        -- "emmet_language_server",
-        "html",
-      },
-    })
-  end,
+	"williamboman/mason.nvim",
+	enabled = true,
+	event = "VeryLazy",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
+	opts = {
+		ui = {
+			border = "single",
+			icons = {
+				package_pending = "  ",
+				package_installed = " 󰄳 ",
+				package_uninstalled = " 󰚌 ",
+			},
+			keymaps = {
+				toggle_server_expand = "<CR>",
+				install_server = "i",
+				update_server = "u",
+				check_server_version = "c",
+				update_all_servers = "U",
+				check_outdated_servers = "C",
+				uninstall_server = "X",
+				cancel_installation = "<C-c>",
+			},
+		},
+		max_concurrent_installers = 10,
+	},
+	config = function()
+		require("mason").setup()
+		local ensure_installed = {}
+		vim.list_extend(ensure_installed, {
+			"stylua",
+		})
+		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"ts_ls",
+				"pylsp",
+				-- "emmet_language_server",
+				"html",
+			},
+		})
+		require("mason-lspconfig").setup_handlers({
+			function(server_name)
+				require("lspconfig")[server_name].setup({
+					capabilities = require("cmp_nvim_lsp").default_capabilities(),
+				})
+			end,
+		})
+	end,
 }
